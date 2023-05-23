3a9386db2917d55df96d3b31516451562f54f343
Danielcard99 <dc2651065@gmail.com>
2023-05-23 05:36:22 -0300

    Adiciona Slider personalizado e Menu responsivo

--- js/functions.js
+++ js/functions.js
@@ -77,4 +77,110 @@ $(function () {
     $("body").css("-o-user-select", "auto");
     $("body").css("user-select", "auto");
   }
+
+  var imgShow = 3;
+  var maxIndex = Math.ceil($(".mini-img-wraper").length / 3) - 1;
+  var curIndex = 0;
+
+  initSlider();
+  navigateSlider();
+  clickSlider();
+  function initSlider() {
+    var amt = $(".mini-img-wraper").length * 33.3;
+    var elScroll = $(".nav-galeria-wraper");
+    var elSingle = $(".mini-img-wraper");
+    elScroll.css("width", amt + "%");
+    elSingle.css("width", 33.3 * (100 / amt) + "%");
+  }
+
+  function navigateSlider() {
+    $(".arrow-right-nav").click(function () {
+      if (curIndex < maxIndex) {
+        curIndex++;
+        var elOff =
+          $(".mini-img-wraper")
+            .eq(curIndex * 3)
+            .offset().left - $(".nav-galeria-wraper").offset().left;
+        $(".nav-galeria").animate({ scrollLeft: elOff + "px" });
+      } else {
+        // console.log("Chegamos até o final");
+      }
+    });
+
+    $(".arrow-left-nav").click(function () {
+      if (curIndex > 0) {
+        curIndex--;
+        var elOff =
+          $(".mini-img-wraper")
+            .eq(curIndex * 3)
+            .offset().left - $(".nav-galeria-wraper").offset().left;
+        $(".nav-galeria").animate({ scrollLeft: elOff + "px" });
+      } else {
+        // console.log("Chegamos até o final");
+      }
+    });
+  }
+
+  function clickSlider() {
+    $(".mini-img-wraper").click(function () {
+      $(".mini-img-wraper").css("background-color", "transparent");
+      $(this).css("background-color", "rgb(210,210,210)");
+      var img = $(this).children().css("background-image");
+      $(".foto-destaque").css("background-image", img);
+    });
+
+    $(".mini-img-wraper").eq(0).click();
+  }
+
+  var directory = "/projeto-05/";
+
+  $("[goto=contato]").click(function () {
+    location.href = directory + "index.html?contato";
+    return false;
+  });
+
+  checkUrl();
+
+  function checkUrl() {
+    var url = location.href.split("/");
+    var curPage = url[url.length - 1].split("?");
+
+    if (curPage[1] != undefined && curPage[1] == "contato") {
+      $("header nav a").css("color", "black");
+      $("footer nav a").css("color", "white");
+      $("[goto=contato]").css("color", "#eb2d2d");
+      $("html,body").animate({ scrollTop: $("#contato").offset().top });
+    }
+  }
+
+  $(".mobile").click(function () {
+    $(this).find("ul").slideToggle();
+  });
+
+  var amtDepoimento = $(".depoimento-single p").length;
+  var curIndex = 0;
+
+  iniciarDepoimentos();
+  navegarDepoimentos();
+
+  function iniciarDepoimentos() {
+    $(".depoimento-single p").hide();
+    $(".depoimento-single p").eq(0).show();
+  }
+
+  function navegarDepoimentos() {
+    $("[next]").click(function () {
+      curIndex++;
+      if (curIndex >= amtDepoimento) curIndex = 0;
+      $(".depoimento-single p").hide();
+      $(".depoimento-single p").eq(curIndex).show();
+    });
+
+    $("[prev]").click(function () {
+      curIndex--;
+      if (curIndex < 0) curIndex = amtDepoimento - 1;
+      $(".depoimento-single p").hide();
+      $(".depoimento-single p").eq(curIndex).show();
+    });
+  }
 });
